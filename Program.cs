27aa0ec329accd819101f86f07d5f536264eac47
Dainis Silamikelis <dainis.silamikelis@gmail.com>
2019-10-12 11:04:05 +0300

    inital HW

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ImageProcessing
+{
+    class Program
+    {
+        static void AddHomeWorkText(int number, string title)
+        {
+            Console.WriteLine("Press {0} to see #{0} homework for {1}", number, title);
+        }
+
+        static void HomeWorkChooser()
+        {
+            var doAnotherHomeWork = false;
+            var GaussianBlur = new GaussianBlur();
+            Console.WriteLine("Home work solutions for Image proccessing course 2019");
+            Console.WriteLine("Press 'X' to close or close the window");
+            Console.WriteLine("Press 'R' to restart");
+            AddHomeWorkText(1, "Gaussian Blur");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.KeyChar == 'x') Environment.Exit(0);
+            if (key.KeyChar == 'r') HomeWorkChooser();
+            if (key.KeyChar == '1') doAnotherHomeWork = GaussianBlur.DoGaussianBlur();
+
+            if (doAnotherHomeWork) HomeWorkChooser();
+            else Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            HomeWorkChooser();
+        }
+    }
+}
---
+++ utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcessing
+{
+    static class utils
+    {
+        public static void handleError(string error, bool debug = false)
+        {
+            Console.Clear();
+            Console.WriteLine("###############################################");
+            if (debug) Console.WriteLine(error);
+            Console.WriteLine("Somewhere, something went terribly wrong. Please contant me dainis.silamikelis@gmail.com");
+            Console.WriteLine("###############################################");
+        }
+
+        public static bool doAnotherHomeWork () {
+            Console.WriteLine("Press Y to do another homework");
+            Console.WriteLine("Press any other key to quit application");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.KeyChar == 'y')
+            {
+                Console.WriteLine();
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
